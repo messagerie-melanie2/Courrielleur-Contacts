d8163f04e2bddac190ad6ff0dcc616ce959e12f7
Ludovic <ludovic@localhost.localdomain>
2021-09-29 12:01:54 +0200

    Paramétrisation du séparateur de boite .-.

--- chrome/contacts/content/contacts/contactsdav-config.js
+++ chrome/contacts/content/contacts/contactsdav-config.js
@@ -208,9 +208,9 @@ function cm2davCarnetExiste(infos) {
 */
 function cm2davUidReduit(uid) {
   
-  let pos=uid.indexOf(".-.");
-  if (-1!=pos)
-    return uid.substr(0,pos);
+  const compos=SplitUserBalp(uid);
+  if (compos && 2==compos.length)
+    return compos[0];
 
   return uid;
 }
